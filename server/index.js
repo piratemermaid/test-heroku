c7eeb085942aec630c03f9e89f76828300b4311d
Ariel <thepiratemermaid4@gmail.com>
2024-01-13 16:13:01 -0700

    Add start scripts for local

--- server/index.js
+++ server/index.js
@@ -13,6 +13,8 @@ app.get('/', function (req, res) {
   res.send('Hello World');
 });
 
-app.listen(process.env.PORT || 3000, function () {
-  console.log('Example app listening on port 3000!');
+const port = process.env.PORT || 3000;
+
+app.listen(port, function () {
+  console.log('Node app listening on port', port);
 });
