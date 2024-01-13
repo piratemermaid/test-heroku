2af953128bf0277b9a828d9b7fcd112e863506c1
Ariel <thepiratemermaid4@gmail.com>
2024-01-13 15:51:25 -0700

    Use process env port

--- server/index.js
+++ server/index.js
@@ -2,17 +2,17 @@ const path = require('path')
 var express = require('express');
 var app = express();
 
-if(process.env.NODE_ENV === 'production') {
+// if(process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client/build')));
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname + '/client/build/index.html'))
   })
-}
+// }
 
 app.get('/', function (req, res) {
   res.send('Hello World');
 });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log('Example app listening on port 3000!');
 });
